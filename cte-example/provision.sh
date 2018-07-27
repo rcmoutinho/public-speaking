8a09afea30b89f59f43ce0de222f0beb7595a50e
Rodrigo Moutinho <rcm1989@gmail.com>
2018-07-27 13:13:59 -0300

    provisioning: database final configuration and database creation

--- cte-example/provision.sh
+++ cte-example/provision.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# ----------------------------
+# CONFIG
+# ----------------------------
+echo "[PROVISIONING CONFIGURATION] ***************************************************"
+
 apt-get update
 
 # postgres
@@ -14,8 +19,30 @@ apt-get clean
 cp /vagrant/conf/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
 chown postgres.postgres /etc/postgresql/9.1/main/pg_hba.conf
 
+cp /vagrant/conf/postgresql.conf /etc/postgresql/9.1/main/postgresql.conf
+chown postgres.postgres /etc/postgresql/9.1/main/postgresql.conf
+
 # restarting services
 service postgresql restart
 
 # checking services
 service postgresql status
+
+# ----------------------------
+# DATABASE
+# ----------------------------
+echo "[PROVISIONING DATABASE] **************************************************"
+
+psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+psql -U postgres -c "REVOKE ALL ON DATABASE postgres from PUBLIC;"
+
+# cleaning older databases
+psql -U postgres -c "DROP DATABASE IF EXISTS totvs_example;"
+
+# creating new ones
+psql -U postgres -c "CREATE DATABASE totvs_example WITH OWNER postgres;"
+psql -U postgres -c "REVOKE ALL ON DATABASE totvs_example from PUBLIC;"
+
+# provisioning use case database
+psql -U postgres -d totvs_example -f /vagrant/database/totvs-example-schema.sql
+psql -U postgres -d totvs_example -f /vagrant/database/totvs-example-data.sql
